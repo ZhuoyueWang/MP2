d9398f83090099fcc810ea33419cfbb3ccfb87bb
Zhuoyue Wang <zhuoyue2@illinois.edu>
2017-10-28 12:20:22 -0500

    4 pt

---
+++ game_4pt.py
@@ -0,0 +1,231 @@
+from search_4pt import *
+import sys
+import math
+import time
+import random
+import pygame
+
+class game:
+    def __init__(self):
+        pygame.init()
+        self.sizeofcell = int(560/8)
+#        self.screen = pygame.display.set_mode((560, 560))
+#        self.screen.fill([255, 255, 255])
+        self.board = 0
+        self.blackchess = 0
+        self.whitechess = 0
+        self.clock = pygame.time.Clock()
+        self.outline = 0
+        self.winner = 0
+        self.status = 0
+        self.turn = 1
+        self.x = 0
+        self.y = 0
+        self.newX = 0
+        self.newY = 0
+        self.totalNode1 = 0
+        self.totalNode2 = 0
+        self.totalTime1 = 0
+        self.totalTime2 = 0
+        self.totalStep1 = 0
+        self.totalStep2 = 0
+        self.eaten = 0
+        # matrix for position of chess, 0 - empty, 1 - black, 2 - white
+        self.matrix = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]]
+
+    def run(self):
+        self.clock.tick(60)
+        # clear the screen
+    #    self.screen.fill([255, 255, 255])
+        self.status = 5
+        if self.status == 5:
+            # Black
+            if self.turn == 1:
+                start = time.clock()
+                self.move(1, 1)
+                self.totalTime1 += (time.clock() - start)
+                self.totalStep1 += 1
+                for i in self.matrix:
+                    print(i)
+                print('\n')
+            elif self.turn == 2:
+                start = time.clock()
+                self.move(1, 2)
+                self.totalTime2 += (time.clock() - start)
+                self.totalStep2 += 1
+                for i in self.matrix:
+                    print(i)
+                print('\n')
+
+        for event in pygame.event.get():
+            # Quit if close the windows
+            if event.type == pygame.QUIT:
+                exit()
+            elif self.status == 0:
+                x, y = event.pos
+                coor_y = math.floor(x / self.sizeofcell)
+                coor_x = math.floor(y / self.sizeofcell)
+                if self.matrix[coor_x][coor_y] == self.turn:
+                    self.status = 1
+                    self.y = math.floor(x / self.sizeofcell)
+                    self.x = math.floor(y / self.sizeofcell)
+            # check whether the selected chess can move, otherwise select other chess
+            elif self.status == 1:
+                x, y = event.pos
+                self.newY = math.floor(x / self.sizeofcell)
+                self.newX = math.floor(y / self.sizeofcell)
+                if self.isabletomove():
+                    self.movechess()
+                    if (self.newX == 4 and self.matrix[self.newX][self.newY] == 1) or (self.newX == 0 and self.matrix[self.newX][self.newY] == 2):
+                        self.status = 3
+                elif self.matrix[self.newX][self.newY] == self.matrix[self.x][self.y]:
+                    self.x = self.newX
+                    self.y = self.newY
+        self.display()
+
+    def display(self):
+        if self.status == 3:
+            if self.turn == 1:
+                print("White Win!")
+                print( 'Black: \n'
+                    'total steps =', self.totalStep1, '\n'
+                      'total expaned nodes =', self.totalNode1, '\n'
+                      'average expaned nodes per move =', self.totalNode1 / self.totalStep1, '\n'
+                      'average time per move =', self.totalTime1 / self.totalStep1, '\n'
+                      'number of captured workers =', self.eaten)
+                print( 'White: \n'
+                    'total steps =', self.totalStep2, '\n'
+                      'total expaned nodes =', self.totalNode2, '\n'
+                      'average expaned nodes per move =', self.totalNode2 / self.totalStep2, '\n'
+                      'average time per move =', self.totalTime2 / self.totalStep2, '\n'
+                      'number of captured workers =', self.eaten)
+                for i in self.matrix:
+                    print(i)
+            else:
+                print("Black Win!")
+                print( 'White: \n'
+                    'total steps =', self.totalStep2, '\n'
+                      'total expaned nodes =', self.totalNode2, '\n'
+                      'average expaned nodes per move =', self.totalNode2 / self.totalStep2, '\n'
+                      'average time per move =', self.totalTime2 / self.totalStep2, '\n'
+                      'number of captured workers =', self.eaten)
+                print( 'Black: \n'
+                    'total steps =', self.totalStep1, '\n'
+                      'total expaned nodes =', self.totalNode1, '\n'
+                      'average expaned nodes per move =', self.totalNode1 / self.totalStep1, '\n'
+                      'average time per move =', self.totalTime1 / self.totalStep1, '\n'
+                      'number of captured workers =', self.eaten)
+                for i in self.matrix:
+                    print(i)
+            sys.exit()
+
+        if self.status == 1:
+            # only downward is acceptable
+            if self.matrix[self.x][self.y] == 1:
+                x1 = self.x + 1
+                y1 = self.y - 1
+                x2 = self.x + 1
+                y2 = self.y + 1
+                x3 = self.x + 1
+                y3 = self.y
+                # left down
+
+            if self.matrix[self.x][self.y] == 2:
+                x1 = self.x - 1
+                y1 = self.y - 1
+                x2 = self.x - 1
+                y2 = self.y + 1
+                x3 = self.x - 1
+                y3 = self.y
+
+    def movechess(self):
+        self.matrix[self.newX][self.newY] = self.matrix[self.x][self.y]
+        self.matrix[self.x][self.y] = 0
+        if self.turn == 1:
+            self.turn = 2
+        elif self.turn == 2:
+            self.turn = 1
+        self.status = 0
+
+    def isabletomove(self):
+        if checkMove == True:
+            return 1
+        return 0
+
+    def checkMove(self):
+        if (self.matrix[self.x][self.y] == 1
+            and self.matrix[self.newX][self.newY] != 1
+            and self.newX - self.x == 1
+            and self.y - 1 <= self.newY <= self.y + 1
+            and not (self.y == self.newY and self.matrix[self.newX][self.newY] == 2)) \
+            or (self.matrix[self.x][self.y] == 2
+                and self.matrix[self.newX][self.newY] != 2
+                and self.x - self.newX == 1
+                and self.y - 1 <= self.newY <= self.y + 1
+                and not (self.y == self.newY and self.matrix[self.newX][self.newY] == 1)):
+            return True
+        return False
+
+
+    def move(self, searchtype, evaluation):
+        if searchtype == 1:
+            return self.move_minimax(evaluation)
+        elif searchtype == 2:
+            return self.move_alphabeta(evaluation)
+
+    def move_minimax(self, function_type):
+        board, nodes, piece = search(self.matrix, self.turn, 3, function_type).minimax()
+        self.moveNode(board,nodes,piece)
+    def move_alphabeta(self, function_type):
+        board, nodes, piece = search(self.matrix, self.turn, 5, function_type).alphabet()
+        self.moveNode(board,nodes,piece)
+
+    def moveNode(self,board,nodes,piece):
+        self.matrix = board.getmatrix()
+        if self.turn == 1:
+            self.totalNode1 += nodes
+            self.turn = 2
+        elif self.turn == 2:
+            self.totalNode2 += nodes
+            self.turn = 1
+        self.eaten = 20-piece
+        if self.isgoalstate():
+            self.status = 3
+
+    def isgoalstate(self):
+        count = 0
+        for i in self.matrix[0]:
+            if i == 2:
+                count += 1
+        if count == 3:
+            return True
+        count = 0
+        for i in self.matrix[4]:
+            if i == 1:
+                count += 1
+        if count == 3:
+            return True
+        count1 = 0
+        count2 = 0
+        for line in self.matrix:
+            for i in line:
+                if i == 1:
+                    count1 += 1
+                elif i == 2:
+                    count2 += 1
+        if count1 <= 2 or count2 <= 2:
+            return True
+        return False
+
+def main():
+    Game = game()
+    while True:
+        Game.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ search_4pt.py
@@ -0,0 +1,283 @@
+import random
+
+class Action:
+    def __init__(self, coordinate, direction, turn):
+        self.coordinate = coordinate
+        self.direction = direction
+        self.turn = turn
+
+class State:
+    def __init__(self, matrix=None, black_position=None, white_position=None, black_num=0,
+         white_num=0, turn=1, function=0, width=10, height=5):
+        self.width = width
+        self.height = height
+        if black_position is None:
+            self.black_positions = []
+        else:
+            self.black_positions = black_position
+        if white_position is None:
+            self.white_positions = []
+        else:
+            self.white_positions = white_position
+        self.black_num = black_num
+        self.white_num = white_num
+        self.turn = turn
+        self.function = function
+        if matrix is not None:
+            for i in range(self.height):
+                for j in range(self.width):
+                    if matrix[i][j] == 1:
+                        self.black_positions.append((i, j))
+                        self.black_num += 1
+                    if matrix[i][j] == 2:
+                        self.white_positions.append((i, j))
+                        self.white_num += 1
+
+    def transfer(self, action):
+        black_pos = list(self.black_positions)
+        white_pos = list(self.white_positions)
+        # black turn
+        if action.turn == 1:
+            if action.coordinate in self.black_positions:
+                index = black_pos.index(action.coordinate)
+                new_pos = self.singleMove(action.coordinate, action.direction, action.turn)
+                black_pos[index] = new_pos
+                if new_pos in self.white_positions:
+                    white_pos.remove(new_pos)
+        # white turn
+        elif action.turn == 2:
+            if action.coordinate in self.white_positions:
+                index = white_pos.index(action.coordinate)
+                new_pos = self.singleMove(action.coordinate, action.direction, action.turn)
+                white_pos[index] = new_pos
+                if new_pos in self.black_positions:
+                    black_pos.remove(new_pos)
+
+        state = State(black_position=black_pos, white_position=white_pos, black_num=self.black_num,
+        white_num=self.white_num, turn=self.switchTurn(action.turn), function=self.function,
+        height=self.height, width=self.width)
+        return state
+
+    def switchTurn(self,turn):
+        if turn == 1:
+            return 2
+        if turn == 2:
+            return 1
+
+    def singleMove(self,initial_pos, direction, turn):
+        if turn == 1:
+            if direction == 1:
+                return initial_pos[0] + 1, initial_pos[1] - 1
+            elif direction == 2:
+                return initial_pos[0] + 1, initial_pos[1]
+            elif direction == 3:
+                return initial_pos[0] + 1, initial_pos[1] + 1
+        elif turn == 2:
+            if direction == 1:
+                return initial_pos[0] - 1, initial_pos[1] - 1
+            elif direction == 2:
+                return initial_pos[0] - 1, initial_pos[1]
+            elif direction == 3:
+                return initial_pos[0] - 1, initial_pos[1] + 1
+
+    def available_actions(self):
+        available_actions = []
+        if self.turn == 1:
+            for i in sorted(self.black_positions, key=lambda p: (p[0], -p[1]), reverse=True):
+                if i[0] != self.height - 1 and i[1] != 0 and (i[0] + 1, i[1] - 1) not in self.black_positions:
+                    available_actions.append(Action(i, 1, 1))
+                if i[0] != self.height - 1 and (i[0] + 1, i[1]) not in self.black_positions and (i[0] + 1, i[1]) not in self.white_positions:
+                    available_actions.append(Action(i, 2, 1))
+                if i[0] != self.height - 1 and i[1] != self.width - 1 and (i[0] + 1, i[1] + 1) not in self.black_positions:
+                    available_actions.append(Action(i, 3, 1))
+        elif self.turn == 2:
+            for i in sorted(self.white_positions, key=lambda p: (p[0], p[1])):
+                if i[0] != 0 and i[1] != 0 and (i[0] - 1, i[1] - 1) not in self.white_positions:
+                    available_actions.append(Action(i, 1, 2))
+                if i[0] != 0 and (i[0] - 1, i[1]) not in self.black_positions and (i[0] - 1, i[1]) not in self.white_positions:
+                    available_actions.append(Action(i, 2, 2))
+                if i[0] != 0 and i[1] != self.width - 1 and (i[0] - 1, i[1] + 1) not in self.white_positions:
+                    available_actions.append(Action(i, 3, 2))
+        return available_actions
+
+    def getmatrix(self):
+        matrix = [[0 for i in range(self.width)] for i in range(self.height)]
+        for item in self.black_positions:
+            matrix[item[0]][item[1]] = 1
+        for item in self.white_positions:
+            matrix[item[0]][item[1]] = 2
+        return matrix
+
+    def myscore(self, turn):
+        if turn == 1:
+            return len(self.black_positions)
+        elif turn == 2:
+            return len(self.white_positions)
+
+    def enemyscore(self, turn):
+        if turn == 1:
+            return len(self.white_positions)
+        elif turn == 2:
+            return len(self.black_positions)
+
+    def offensive(self, turn):
+        return 6*(30-self.enemyscore(turn)) +random.random()
+
+    def defensive(self, turn):
+        return 6*self.myscore(turn) + random.random()
+
+    def offensive2(self, turn):
+        return 2*self.myscore(turn) - 6*self.enemyscore(turn)+ random.random()
+
+    def defensive2(self, turn):
+        return 6*self.myscore(turn) - 2*self.enemyscore(turn)+ random.random()
+
+    def choice(self, turn):
+        if self.function == 0:
+            return 0
+        elif self.function == 1:
+            return self.offensive(turn)
+        elif self.function == 2:
+            return self.defensive(turn)
+        elif self.function == 3:
+            return self.offensive2(turn)
+        elif self.function == 4:
+            return self.defensive2(turn)
+
+    def isgoalstate(self, type=0):
+        if type == 0:
+            if 0 in [item[0] for item in self.white_positions] or len(self.black_positions) == 0:
+                return 2
+            if self.height - 1 in [item[0] for item in self.black_positions] or len(self.white_positions) == 0:
+                return 1
+            return 0
+        else:
+            count = 0
+            for i in self.black_positions:
+                if i[0] == 4:
+                    count += 1
+            if count == 3:
+                return True
+            count = 0
+            for i in self.white_positions:
+                if i[0] == 0:
+                    count += 1
+            if count == 3:
+                return True
+            if len(self.black_positions) <= 2 or len(self.white_positions) <= 2:
+                return True
+        return False
+
+class search:
+    def __init__(self, matrix, turn, depth, function, type=0):
+        self.matrix = matrix
+        self.turn = turn
+        self.maxdepth = depth
+        self.function = function
+        self.type = type
+        self.nodes = 0
+        self.piece_num = 0
+
+    def minimax_MAX(self, state, depth):
+        if depth == self.maxdepth or state.isgoalstate(1) != 0:
+            #print("choice", state.choice(self.turn))
+            return state.choice(self.turn)
+        result= -float("inf")
+        for action in state.available_actions():
+            # print(state.transfer(action).getmatrix())
+            result= max(result, self.minimax_MIN(state.transfer(action), depth + 1))
+            self.nodes += 1
+        return result
+
+    def minimax_MIN(self, state, depth):
+        if depth == self.maxdepth or state.isgoalstate(1) != 0:
+            #print("choice", state.choice(self.turn))
+            return state.choice(self.turn)
+        result= float("inf")
+        for action in state.available_actions():
+            result= min(result, self.minimax_MAX(state.transfer(action), depth + 1))
+            self.nodes += 1
+        return result
+
+    def minimax(self):
+        final_action = None
+        if self.type == 0:
+            initialstate = State(matrix=self.matrix, turn=self.turn, function=self.function)
+        else:
+            initialstate = State(matrix=self.matrix, turn=self.turn, function=self.function, height=5, width=10)
+        result= -float("inf")
+        for action in initialstate.available_actions():
+            self.nodes += 1
+            new_state = initialstate.transfer(action)
+            if new_state.isgoalstate(1):
+                final_action = action
+                break
+            minresult = self.minimax_MIN(new_state, 1)
+            if minresult > result:
+                final_action = action
+                result= minresult
+        if self.turn == 1:
+            temp = initialstate.transfer(final_action)
+            self.piece_num = temp.white_num
+        elif self.turn == 2:
+            temp = initialstate.transfer(final_action)
+            self.piece_num = temp.black_num
+        return initialstate.transfer(final_action), self.nodes, self.piece_num
+
+
+    def alphabeta_MAX(self, state, alpha, beta, depth):
+        if depth == self.maxdepth or state.isgoalstate() != 0:
+            return state.choice(self.turn)
+        result = -float("inf")
+        actions = state.available_actions()
+        actions = sorted(state.available_actions(), key=lambda action: self.none(action, state), reverse=True)
+        for action in actions:
+            self.nodes += 1
+            result = max(result, self.alphabeta_MIN(state.transfer(action), alpha, beta, depth + 1))
+            if result >= beta:
+                return result
+            alpha = max(alpha, result)
+        return result
+
+    def alphabeta_MIN(self, state, alpha, beta, depth):
+        if depth == self.maxdepth or state.isgoalstate() != 0:
+            return state.choice(self.turn)
+        result = float("inf")
+        actions = state.available_actions()
+        actions = sorted(state.available_actions(), key=lambda action: self.none(action, state))
+        for action in actions:
+            self.nodes += 1
+
+            result = min(result, self.alphabeta_MAX(state.transfer(action), alpha, beta, depth + 1))
+            if result <= alpha:
+                return result
+            beta = min(beta, result)
+        return result
+
+    def alphabet(self):
+        final_action = None
+        if self.type == 0:
+            initialstate = State(matrix=self.matrix, turn=self.turn, function=self.function)
+        else:
+            initialstate = State(matrix=self.matrix, turn=self.turn, function=self.function, height=5, width=10)
+        result = -float("inf")
+        for action in initialstate.available_actions():
+            self.nodes += 1
+            new_state = initialstate.transfer(action)
+            if new_state.isgoalstate():
+                final_action = action
+                break
+            minresult = self.alphabeta_MIN(new_state, -float("inf"), float("inf"), 1)
+            if minresult > result:
+                final_action = action
+                result = minresult
+        if self.turn == 1:
+            temp = initialstate.transfer(final_action)
+            self.piece_num = temp.white_num
+        elif self.turn == 2:
+            temp = initialstate.transfer(final_action)
+            self.piece_num = temp.black_num
+        return initialstate.transfer(final_action), self.nodes, self.piece_num
+
+    def none(self, action, state):
+        return 0
